c718b8a100e40be075e579bacb60985e2a66f390
Jesse Weaver <pianohacker@gmail.com>
2013-04-25 20:59:05 -0600

    Print results, optional verbose flag

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -6,8 +7,17 @@
 #include "magic.h"
 
 int main(int argc, char **argv) {
+	char *progname = argv[0];
+	bool verbose = false;
+
+	if (argc >= 2 && strcmp(argv[1], "-v") == 0) {
+		verbose = true;
+		argc--;
+		argv++;
+	}
+
 	if (argc > 2) {
-		fprintf(stderr, "Usage: %s [FILENAME]\n", argv[0]);
+		fprintf(stderr, "Usage: %s [FILENAME]\n", progname);
 		return 1;
 	}
 
@@ -27,27 +37,29 @@ int main(int argc, char **argv) {
 
 	loadData(input, &taskSize, &tasks, &machineSize, &machines);
 
-	printf("%d tasks:\n", taskSize);
+	if (verbose) {
+		printf("%d tasks:\n", taskSize);
 
-	for (int i = 0; i < taskSize; i++) {
-		printf("  %d\n", tasks[i]);
-	}
-	
-	printf("\n%d machines:\n", machineSize);
+		for (int i = 0; i < taskSize; i++) {
+			printf("  %d\n", tasks[i]);
+		}
+		
+		printf("\n%d machines:\n", machineSize);
 
-	for (int i = 0; i < machineSize; i++) {
-		printf("  %d\n", machines[i]);
+		for (int i = 0; i < machineSize; i++) {
+			printf("  %d\n", machines[i]);
+		}
 	}
 
 	int **machineTasks = computeTime(tasks, machines, taskSize, machineSize);
 
-	puts("\nTask assignments:\n");
+	if (verbose) puts("\nTask assignments:\n");
 
 	for (int i = 0; i < machineSize; i++) {
-		printf("  Machine %d:", i + 1);
+		if (verbose) printf("  Machine %d has tasks:", i + 1);
 
 		for (int j = 0; machineTasks[i][j]; j++) {
-			printf(" %d", machineTasks[i][j] + 1);
+			printf("%s%d", (i ? " " : ""), machineTasks[i][j] + 1);
 		}
 
 		puts("\n");
