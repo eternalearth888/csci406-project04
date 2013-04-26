7c2a42df44de4230b522673d0bdd9c12e65c42bf
eternalearth888 <mdeslis@gmail.com>
2013-04-25 21:10:43 -0600

    Current version of algorithm. Produces output, though it may not be correct atm

--- src/magic.c
+++ src/magic.c
@@ -1,44 +1,41 @@
 #include <stdio.h> 
 #include <stdlib.h>
-//return the time after adding taskTime to currentTasks at machineSpeed
-float timeAfterTask (int *currentTasks, int machineSpeed, int taskTime);
-
-//will append the task to the array of assigned tasks
-void appendToMachine (int *currentTasks, int task);
-
-float computeTime (int *tasks, int *machines, int taskSize, int machineSize); 
 
 /*
 // # % -- ; main
 */
 
-
-float timeAfterTask(int* currentTasks, int machineSpeed, int taskTime) {
+//return the time after adding taskTime to currentTasks at machineSpeed
+float timeAfterTask(int* currentTasks, int machineSpeed, int* tasks, int taskId) {
 	int amount = 0;
 	int numTasksAssigned = 0;
 	for (numTasksAssigned; currentTasks[numTasksAssigned] != -1; numTasksAssigned++) {
 	}
 	// now we have number of tasks currently assigned, ie the next task goes into index of numTasksAssigned
 	for (int i = 0; i < numTasksAssigned; i++) {
-		amount += currentTasks[i];
+		amount += tasks[currentTasks[i]];
 	}
-	amount += taskTime;
+	amount += tasks[taskId];
 
 
 	return (float)(amount/machineSpeed);
 }
-
-void appendToMachine(int* currentTasks, int task) {
+//will append the task to the array of assigned tasks
+void appendToMachine(int* currentTasks, int tasksID) {
 	int numTasksAssigned = 0;
 	for (numTasksAssigned; currentTasks[numTasksAssigned] != -1; numTasksAssigned++) {
 	}
-	currentTasks[numTasksAssigned] = task;
+	currentTasks[numTasksAssigned] = tasksID;
 }
 
-float computeTime(int* tasks, int* machines, int taskSize, int machineSize) {
+int** computeTime(int* tasks, int* machines, int taskSize, int machineSize) {
 	float currentWorstTime = 0; // the amount of time it takes to finish the task assigned to their respective machines
 
-	int machinesTasks[machineSize][taskSize]; // each index corresponds to a machine, and the array it cotains is the tasks assigned to that machine
+	int **machinesTasks = malloc(machineSize * sizeof(int*));
+	for (int i = 0; i < machineSize; i++){
+		machinesTasks[i] = malloc(taskSize * sizeof(int));
+	}
+	//int machinesTasks[machineSize][taskSize]; // each index corresponds to a machine, and the array it cotains is the tasks assigned to that machine
 	for (int i = 0; i < machineSize; i++) {
 		for (int j = 0; j < taskSize; j++) {
 			machinesTasks[i][j] = -1;
@@ -48,8 +45,8 @@ float computeTime(int* tasks, int* machines, int taskSize, int machineSize) {
 	for (int i = 0; i < taskSize; i++) {
 		int hasAssigned = 0; //bool for checking if we have assigned a task
 		for (int j = 0; j < machineSize; j++) {
-			if (timeAfterTask(machinesTasks[j], machines[j], tasks[i]) <= currentWorstTime && hasAssigned == 0) {
-				appendToMachine(machinesTasks[j], tasks[i]);
+			if (timeAfterTask(machinesTasks[j], machines[j], tasks, i) <= currentWorstTime && hasAssigned == 0) {
+				appendToMachine(machinesTasks[j], i);
 				hasAssigned = 1;
 			}
 		}
@@ -57,17 +54,17 @@ float computeTime(int* tasks, int* machines, int taskSize, int machineSize) {
 				float lowestIncrease = 999999999999999; // A large number
 				int lowestMachine = 0;
 				for (int j = 0; j < machineSize; j++) {
-					if (timeAfterTask(machinesTasks[j], machines[j], tasks[i]) < lowestIncrease) {
-						lowestIncrease = timeAfterTask(machinesTasks[j], machines[j], tasks[i]);
+					if (timeAfterTask(machinesTasks[j], machines[j], tasks, i) < lowestIncrease) {
+						lowestIncrease = timeAfterTask(machinesTasks[j], machines[j], tasks, i);
 						lowestMachine = j;
 					}
 				}
-		    currentWorstTime = currentWorstTime + timeAfterTask(machinesTasks[lowestMachine], machines[lowestMachine], tasks[i]);
-			appendToMachine(machinesTasks[lowestMachine], tasks[i]);
+		    currentWorstTime = currentWorstTime + timeAfterTask(machinesTasks[lowestMachine], machines[lowestMachine], tasks, i);
+			appendToMachine(machinesTasks[lowestMachine], i);
 		}
 	}
 
-	float barryManillow = currentWorstTime;
+	int** barryManillow = machinesTasks;
 
 	return barryManillow;
 }
