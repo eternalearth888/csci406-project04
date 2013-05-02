f731be5cda899980b54d150bca8f9ee31411b061
codfish92 <akoellin@mines.edu>
2013-05-02 13:47:42 -0600

    made script to make sure you use all tasks

--- src/verify.py
+++ src/verify.py
@@ -21,16 +21,24 @@ for i in range(0, len(text)-1):#for each machine
 			derp.append(int(number)) #add the task to the machines assigned 
 	tasksAssigned.append(derp)  #add the array of tasks to machine
 givenCost = float(text[len(text)-1].split()[0]) # get the cost the other group said they got
+tasksUsed = [] # will hold all the indexs of the tasks called, this will later be used to verfiy that all tasks are called
 maxCost = 0 #intilize 0
 #get the cost with the tasks assigned to the respective machine
 for machine in range(0, len(tasksAssigned)):
 	machineTime = 0 #assume machine has 0
 	for task in tasksAssigned[machine]:
+		if tasksUsed.count(task) == 0:
+			tasksUsed.append(task) #will indicate that the task has been assinged
 		machineTime += (float(taskTimes[task])/machineSpeeds[machine]) # add the time it takes to run the task on that machine
 	if maxCost < machineTime: #if that machine takes longer than the current max
 		maxCost = machineTime #make it the max
 
-if maxCost != givenCost: # if their cost is not equal to the cost calculated above
+taskIndexSum = 0#adds up all the indexs of the tasks into a sum. if they go through all 20 tasks, this sum should be 190, with the length of tasks used being 20
+for taskIndex in tasksUsed:
+	taskIndexSum += taskIndex 
+
+
+if maxCost != givenCost or taskIndexSum != 190 or len(tasksUsed) != 20: # if their cost is not equal to the cost calculated above, or if they havent used all of the tasks, or if they used the same task twice
 	print "\nNOT VALID\n" #its not valid
 else:
 	print "\nVALID\n" #otherwise it is
